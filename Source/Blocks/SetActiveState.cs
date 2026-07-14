1116e9ae1c4403220cee8795893efe50be3fc8ec
Chris Collins <29593332+christides11@users.noreply.github.com>
2026-07-13 22:22:34 -0700

    feat: SetActiveState block

---
+++ Source/Blocks/SetActiveState.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace ct.ActionBlocks.Blocks
+{
+    [Serializable]
+    public class SetActiveState : ActionBlockBase
+    {
+        public GameObject gameObject;
+        public bool activeState = true;
+        
+        public override bool Execute(GameObject gameObject, ref ActionBlockSequence sequence)
+        {
+            gameObject.SetActive(activeState);
+            return true;
+        }
+    }
+}
